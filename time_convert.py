a76383804c47e2b1f2813e988a4237e6ad8655b7
Jacob Hegy <85857129+Jacob-Hegy@users.noreply.github.com>
2024-05-24 21:34:50 -0500

    Uploading time_convert.py

---
+++ time_convert.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import argparse
+from datetime import *
+
+parser = argparse.ArgumentParser(description = "Convert dwLowDateTime and dwHighDateTime to a UTC timestamp")
+parser.add_argument('-x', '--low', help = "dwLowDateTime", type = int, required = True)
+parser.add_argument('-y', '--high', help = 'dwHighDateTime', type = int, required = True)
+
+args = parser.parse_args()
+
+EPOCH_FILETIME = 116444736000000000
+
+class Timestamp:
+	def __init__(self, ldt, hdt):
+		self.dw_ldt = ldt
+		self.dw_hdt = hdt
+
+	def getTime(self):
+		return datetime.fromtimestamp((((self.dw_hdt << 32) | (self.dw_ldt & 0xFFFFFFFF)) - EPOCH_FILETIME)  / (10 ** 7), timezone.utc)
+
+def main():
+	print(Timestamp(args.low, args.high).getTime())
+
+if __name__ == "__main__":
+	main()
